1bb147a5bc7f2adc73630694092d1339bdeb384a
xiaoming <934933088@qq.com>
2018-09-05 11:17:49 +0800

    暑假项目

--- database/migrations/2018_08_24_010503_create_third_types_table.php
+++ database/migrations/2018_08_24_010503_create_third_types_table.php
@@ -16,7 +16,7 @@ class CreateThirdTypesTable extends Migration
         Schema::create('third_types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('type_id')->nullable();
+            //$table->integer('type_id')->nullable();
             $table->integer('secondtype_id');
             $table->timestamps();
         });
