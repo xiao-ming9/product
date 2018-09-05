0155e9876e8e88f3cefd33cc35b1b0dd3eb03044
xiaoming <934933088@qq.com>
2018-09-05 11:27:15 +0800

    暑假项目

--- app/Admin/Controllers/ThirdTypeController.php
+++ app/Admin/Controllers/ThirdTypeController.php
@@ -129,7 +129,7 @@ class ThirdTypeController extends Controller
         $form->text('name', '三级分类名称');
         $form->select('secondtype_id','父级分类')->options('/api/thirdtype');
         //$form->number('secondtype_id', 'Secondtype id');
-        //$form->number('type_id', 'Type id');
+        $form->hidden('type_id')->value(null);
         return $form;
     }
 }
